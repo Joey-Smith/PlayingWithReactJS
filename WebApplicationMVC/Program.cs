db4ee209945498842bb92eca9b9fbc722f6aff8f
Joey Smith <joey.m.smith@outlook.com>
2024-08-16 20:45:32 -0700

    #1 ReactJS Tutorial

--- WebApplicationMVC/Program.cs
+++ WebApplicationMVC/Program.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using WebApplicationMVC.Data;
 using WebApplicationMVC.Services;
+using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +20,14 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.Configure<EmailConfiguration>(builder.Configuration.GetSection("EmailConfiguration"));
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddReact();
+
+// Make sure a JS engine is registered, or you will get an error!
+builder.Services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+  .AddV8();
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddTransient<IEmailSender, EmailSender>();
@@ -34,6 +46,27 @@ else
 }
 
 app.UseHttpsRedirection();
+
+// Initialise ReactJS.NET. Must be before static files.
+app.UseReact(config =>
+{
+    // If you want to use server-side rendering of React components,
+    // add all the necessary JavaScript files here. This includes
+    // your components as well as all of their dependencies.
+    // See http://reactjs.net/ for more information. Example:
+    config
+      .AddScript("~/js/remarkable.min.js")
+      .AddScript("~/js/tutorial.jsx");
+
+    // If you use an external build too (for example, Babel, Webpack,
+    // Browserify or Gulp), you can improve performance by disabling
+    // ReactJS.NET's version of Babel and loading the pre-transpiled
+    // scripts. Example:
+    //config
+    //  .SetLoadBabel(false)
+    //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+});
+
 app.UseStaticFiles();
 
 app.UseRouting();
