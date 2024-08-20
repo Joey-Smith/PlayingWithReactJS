391738cd8b2debf32b79c7e5899ebe756ac39f37
Joey Smith <joey.m.smith@outlook.com>
2024-08-19 18:20:49 -0700

    #8 Completed Tutorial

---
+++ WebApplicationMVC/Controllers/TypeScriptController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class TypeScriptController : Controller
+    {
+        public IActionResult TypeScript()
+        {
+            return View();
+        }
+    }
+}
